8920c7b681bcf6b8386494662dc4430adffe3888
Collin <collincodes9@gmail.com>
2025-05-14 11:45:59 -0500

    pushing working progress of a refactor to repo to save progress, will pick this back up in the future

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -4,6 +4,7 @@ import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 
+// from main below
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
@@ -37,4 +38,43 @@ function ProjectCards(props) {
     </Card>
   );
 }
+
+
+
+
+// new from copilot below
+// function ProjectCards(props) {
+//   return (
+//     <Card className="project-card-view">
+//       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+//       <Card.Body>
+//         <Card.Title className="project-title">{props.title}</Card.Title>
+//         <Card.Text className="project-description">
+//           {props.description}
+//         </Card.Text>
+//         <div className="button-container">
+//           {props.ghLink && (
+//             <Button variant="primary" href={props.ghLink} target="_blank">
+//               <BsGithub /> &nbsp;
+//               {props.isBlog ? "Blog" : "GitHub"}
+//             </Button>
+//           )}
+//           {props.demoLink && (
+//             <Button
+//               variant="primary"
+//               href={props.demoLink}
+//               target="_blank"
+//               style={{ marginLeft: "10px" }}
+//             >
+//               <CgWebsite /> &nbsp;
+//               {"Demo"}
+//             </Button>
+//           )}
+//         </div>
+//       </Card.Body>
+//     </Card>
+//   );
+// }
+
+
 export default ProjectCards;
